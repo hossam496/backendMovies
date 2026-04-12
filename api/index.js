43fb5662292aab717417fd22b878c4fcdc3fc990
Hossam Albasuony <albasuonyh@gmail.com>
2026-04-12 21:20:49 +0200

    fix CORS double slash and explicit origin

--- api/index.js
+++ api/index.js
@@ -14,10 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://booking-movies.vercel.app"
-  ],
+  origin: "https://booking-movies.vercel.app",
   credentials: true
 }));
 
--- server.js
+++ server.js
@@ -14,11 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://booking-movies.vercel.app"
-    ],
-    credentials: true
+  origin: "https://booking-movies.vercel.app",
+  credentials: true
 }));
 
 // DB
